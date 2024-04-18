99ba86e55b20d86f8fa1be97b495abe2fbd88cba
min khant <minkhant4287@gmail.com>
2024-04-18 22:24:51 +0630

    Animated Search Bar

---
+++ app.js
@@ -0,0 +1,5 @@
+let searchBar = document.querySelector(".container");
+const magnifier = document.querySelector(".magnifier");
+magnifier.addEventListener("click", () => {
+    searchBar.classList.toggle("active");
+})
